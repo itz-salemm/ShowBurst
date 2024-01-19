aa4f54147f4232e6fa4bd26e9d2fb945631c46a6
Salem Olorundare <salemolorundare@gmail.com>
2024-01-19 17:40:15 +0100

    linked to AWS S3

--- Stream/index.js
+++ Stream/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const fs = require("fs");
+const AWS = require("aws-sdk");
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION_NAME,
+});
 
 const app = express();
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const key = "firstVideo.mp4";
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -13,22 +23,38 @@ app.get("/video", (req, res) => {
     res.status(400).send("Requires range header");
   }
 
-  const videoPath = "test.mp4";
-  const videoSize = fs.statSync("test.mp4").size;
-  const CHUNK_SIZE = 10 ** 6;
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-  const contentLength = end - start + 1;
+  //const videoPath = "s3://${bucketName}/${key}";
+  s3.headObject({ Bucket: bucketName, Key: key }, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error getting video information from S3");
+      return;
+    }
 
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
-  res.writeHead(206, headers);
-  const videoStream = fs.createReadStream(videoPath, { start, end });
-  videoStream.pipe(res);
+    const videoSize = data.ContentLength;
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, headers);
+
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+      Range: `bytes=${start}-${end}`,
+    };
+
+    const videoStream = s3.getObject(params).createReadStream();
+    videoStream.pipe(res);
+  });
 });
 
 app.listen(8000, () => {
