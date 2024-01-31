011df01194e65fdac984fcafc6a40f5eb970de71
Salem Olorundare <salemolorundare@gmail.com>
2024-01-31 14:52:02 +0100

    added register route

--- Authentication/index.ts
+++ Authentication/index.ts
@@ -3,6 +3,9 @@ import { Application } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import { User } from "./models/userModel";
+import jwt from "jsonwebtoken";
+
 // Create the express app and  import the type of app from express;
 const app: Application = express();
 
@@ -22,6 +25,60 @@ const PORT: number = 4000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome To JWT Authentication </h1>");
+});
+
+app.post("/auth/register", async (req, res) => {
+  try {
+    // ** Get The User Data From Body ;
+    const user = req.body;
+
+    // ** destructure the information from user;
+    const { name, email, password } = user;
+
+    // ** Check the email all ready exist  in database or not ;
+    // ** Import the user model from "./models/user";
+
+    const isEmailAllReadyExist = await User.findOne({
+      email: email,
+    });
+
+    // ** Add a condition if the user exist we will send the response as email all ready exist
+    if (isEmailAllReadyExist) {
+      res.status(400).json({
+        status: 400,
+        message: "Email all ready in use",
+      });
+      return;
+    }
+
+    // ** if not create a new user ;
+    // !! Don't save the password as plain text in db . I am saving just for demonstration.
+    // ** You can use bcrypt to hash the plain password.
+
+    // now create the user;
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    // Send the newUser as  response;
+    res.status(200).json({
+      status: 201,
+      success: true,
+      message: " User created Successfully",
+      user: newUser,
+    });
+  } catch (error: any) {
+    // console the error to debug
+    console.log(error);
+
+    // Send the error message to the client
+    res.status(400).json({
+      status: 400,
+      message: error.message.toString(),
+    });
+  }
 });
 
 // Listen the server
