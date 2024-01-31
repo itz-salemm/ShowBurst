4ddb3c2fa4f3e5582a060ad5926865940b67f584
Salem Olorundare <salemolorundare@gmail.com>
2024-01-31 14:52:22 +0100

    added login route

--- Authentication/index.ts
+++ Authentication/index.ts
@@ -81,6 +81,81 @@ app.post("/auth/register", async (req, res) => {
   }
 });
 
+app.post("/auth/login", async (req, res) => {
+  try {
+    // ** Get The User Data From Body ;
+    const user = req.body;
+
+    // ** destructure the information from user;
+    const { email, password } = user;
+
+    // ** Check the (email/user) exist  in database or not ;
+    const isUserExist = await User.findOne({
+      email: email,
+    });
+
+    // ** if there is not any user we will send user not found;
+    if (!isUserExist) {
+      res.status(404).json({
+        status: 404,
+        success: false,
+        message: "User not found",
+      });
+      return;
+    }
+
+    // ** if the (user) exist  in database we will check the password is valid or not ;
+    // **  compare the password in db and the password sended in the request body
+
+    const isPasswordMatched = isUserExist?.password === password;
+
+    // ** if not matched send response that wrong password;
+
+    if (!isPasswordMatched) {
+      res.status(400).json({
+        status: 400,
+        success: false,
+        message: "wrong password",
+      });
+      return;
+    }
+
+    // ** if the email and password is valid create a token
+
+    /*
+      To create a token JsonWebToken (JWT) receive's 3 parameter
+      1. Payload -  This contains the claims or data you want to include in the token.
+      2. Secret Key - A secure key known only to the server used for signing the token.
+      3. expiration -  Additional settings like token expiration or algorithm selection.
+      */
+
+    // !! Don't Provide the secret openly, keep it in the .env file. I am Keeping Open just for demonstration
+
+    // ** This is our JWT Token
+    const token = jwt.sign(
+      { _id: isUserExist?._id, email: isUserExist?.email },
+      "pocess.env.SECRET",
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // send the response
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "login success",
+      token: token,
+    });
+  } catch (error: any) {
+    // Send the error message to the client
+    res.status(400).json({
+      status: 400,
+      message: error.message.toString(),
+    });
+  }
+});
+
 // Listen the server
 app.listen(PORT, async () => {
   console.log(`🗄️  Server Fire on http:localhost//${PORT}`);
