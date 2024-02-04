f3032c2b17cb00a25093815b04b3e626fae3e668
Salem Olorundare <salemolorundare@gmail.com>
2024-02-04 18:39:48 +0100

    added admin authorization feature

--- Authentication/middlewares/verifyUser.ts
+++ Authentication/middlewares/verifyUser.ts
@@ -40,3 +40,38 @@ export const verifyUser = (
     return res.status(401).json({ message: "Invalid token" });
   }
 };
+
+export const verifyAdmin = (
+  req: RequestWithUserData,
+  res: Response,
+  next: NextFunction
+) => {
+  //const token = req.header("showBurstAuthToken");
+  const token = req.cookies.showBurstAuthToken;
+  //const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Authorization token not provided" });
+  }
+
+  const secret: string | undefined = process.env.SECRET;
+
+  if (!secret) {
+    throw new Error("JWT secret is not defined in environment variables");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, secret);
+    req.userData = decodedToken as { userId: string };
+    console.log(req.body.role);
+    if (req.body.role != "user") {
+      return res.status(401).json({ message: "Unauthorized" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
